47fed45192d0a266ac05b60107b8e7cca5deba6b
Eduardo Dorantes <eduardo.dorantes.07@gmail.com>
2021-08-24 19:46:11 -0500

    RN 0.65.1

--- metro.config.js
+++ metro.config.js
@@ -5,24 +5,25 @@
  * @format
  */
 
-const { getDefaultConfig } = require("metro-config");
+ const { getDefaultConfig } = require("metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { assetExts }
-  } = await getDefaultConfig();
-
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: [...assetExts, "obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject", "gif"]
-    }
-  }
-})();
+ module.exports = (async () => {
+   const {
+     resolver: { assetExts }
+   } = await getDefaultConfig();
+ 
+   return {
+     transformer: {
+       getTransformOptions: async () => ({
+         transform: {
+           experimentalImportSupport: false,
+           inlineRequires: true,
+         },
+       }),
+     },
+     resolver: {
+       assetExts: [...assetExts, "obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject", "gif"]
+     }
+   }
+ })();
+ 
