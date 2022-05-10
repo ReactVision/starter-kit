7aaf5e7bd31a8e9d8da188f8bf9c39ae2bf381b1
Pratt Vaidya <pratt.vaidya@ttt.studio>
2022-05-09 17:06:59 -0700

    Replace ViroContants w/ ViroTrackingStateConstants

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
   ViroText,
-  ViroConstants,
+  ViroTrackingStateConstants,
   ViroARSceneNavigator,
 } from '@viro-community/react-viro';
 
@@ -12,9 +12,9 @@ const HelloWorldSceneAR = () => {
 
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
-    if (state === ViroConstants.TRACKING_NORMAL) {
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
       setText('Hello World!');
-    } else if (state === ViroConstants.TRACKING_NONE) {
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
       // Handle loss of tracking
     }
   }
