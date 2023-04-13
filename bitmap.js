a39e5265f4be2542819e53031632636fa91203dc
Mauricio Juárez Ibáñez <mji2000.741@gmail.com>
2023-04-12 18:39:52 -0600

    cambios en bitmap

--- bitmap.js
+++ bitmap.js
@@ -2,9 +2,15 @@
 
 const fs = require('fs'),
   path = require('path');
-const width = 1000,
-  height = 1000,
+// change here to generate qr with given id
+const id = 121;
+
+const width = 450,
+  height = 450,
   bpp = 24; // Bits per pixel
+const marco = 75;
+const rWidth = width + 2*marco, rHeight = height + 2*marco;
+
 process.on('unhandledRejection', error => {
   console.error('unhandledRejection', error.message);
 });
@@ -12,8 +18,8 @@ process.on('unhandledRejection', error => {
 (async function () {
   // https://en.wikipedia.org/wiki/BMP_file_format
   // Each row in the Pixel array is padded to a multiple of 4 bytes in size
-  let rowSize = Math.floor((bpp * width + 31) / 32) * 4;
-  let pixelArraySize = rowSize * height;
+  let rowSize = Math.floor((bpp * rWidth + 31) / 32) * 4;
+  let pixelArraySize = rowSize * rHeight;
 
   let BMP = new DataView(new ArrayBuffer(54));
 
@@ -28,8 +34,8 @@ process.on('unhandledRejection', error => {
 
   // DIB header, Windows BITMAPINFOHEADER
   BMP.setUint32(14, 40, true); // DIB header size (=40 bytes)
-  BMP.setUint32(18, width, true); // Width in pixels
-  BMP.setUint32(22, height, true); // Height in pixels
+  BMP.setUint32(18, rWidth, true); // Width in pixels
+  BMP.setUint32(22, rHeight, true); // Height in pixels
   BMP.setUint16(26, 1, true); // Number of color planes (1)
   BMP.setUint16(28, bpp, true); // Bits per pixel
   BMP.setUint32(30, 0, true); // No compression (0)
@@ -45,31 +51,59 @@ process.on('unhandledRejection', error => {
   let i = 0;
   let byesPerPixel = (bpp / 8) | 0;
 
+  const getCuadrante = (w,h) => {
+    if(h<height/3){
+      if(w<width/3){
+        return 0
+      }else if(w<width*2/3){
+        return 1
+      }else{
+        return 2
+      }
+    }else if(h<height*2/3){
+      if(w<width/3){
+        return 3
+      }else if(w<width*2/3){
+        return 4
+      }else{
+        return 5
+      }
+    }else{
+      if(w<width/3){
+        return 6
+      }else if(w<width*2/3){
+        return 7
+      }else{
+        return 8
+      }
+    }
+  }
   // From bottom row to the top
-  //   for (let y = height - 1; y >= 0; y--) {
-  //     for (let x = 0; x < width; x++) {
-  //       bmpData.setUint8(i, 0); // B
-  //       bmpData.setUint8(i + 1, 0); // G
-  //       bmpData.setUint8(i + 2, 255); // R
-
-  //       i += byesPerPixel;
-  //     }
-  //     i = rowSize * (height - y);
-  //   }
-
-  bmpData.setUint8(0, 255); // B
-  bmpData.setUint8(1, 255); // B
-  bmpData.setUint8(2, 255); // B
-
-  bmpData.setUint8(3, 255); // B
-  bmpData.setUint8(4, 255); // B
-  bmpData.setUint8(5, 255); // B
+   for (let y = rHeight; y >= 0; y--) {
+     for (let x = 0; x < rWidth; x++) {
+      if(x<marco || y<marco || x>marco+width || y>marco+height){
+        bmpData.setUint8(i, 0); // B
+        bmpData.setUint8(i + 1, 0); // G
+        bmpData.setUint8(i + 2, 255); // R
+      } else if((id & 1 << getCuadrante(x-marco,y-marco)) != 0){
+       bmpData.setUint8(i, 0); // B
+       bmpData.setUint8(i + 1, 0); // G
+       bmpData.setUint8(i + 2, 0); // R
+      } else {
+      bmpData.setUint8(i, 255); // B
+      bmpData.setUint8(i + 1, 255); // G
+      bmpData.setUint8(i + 2, 255); // R
+     }
+     i += byesPerPixel;
+   }
+   i = rowSize * (rHeight - y);
+  }
 
   let buffer = Buffer.concat([
     Buffer.from(BMP.buffer),
     Buffer.from(bmpData.buffer),
   ]);
 
-  let outFile = __dirname + '/out.png';
+  let outFile = __dirname + '/qrs/' + id +  '.png';
   fs.writeFileSync(outFile, buffer);
 })();
