62edc012c1f124efe0259c89df0b430909f9b3e7
Robert Colley <robert.colley04@gmail.com>
2024-02-02 09:21:00 -0500

    Fix issues and build on Android Emulator

--- android/app/src/main/java/com/virostarterkit/MainApplication.kt
+++ android/app/src/main/java/com/virostarterkit/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.viromedia.bridge.ReactViroPackage
 
 class MainApplication : Application(), ReactApplication {
 
